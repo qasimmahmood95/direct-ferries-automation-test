010b797212d0ac3852c8d40f6b66f0be7f2bd842
Direct Ferries <emmab@directferries.com>
2024-08-20 08:38:15 +0000

    update to include dealfinder version

--- src/methods/navigation-methods.js
+++ src/methods/navigation-methods.js
@@ -3,7 +3,7 @@ const { Selector, t } = require('testcafe');
 module.exports = {
     onHomepage: async function () {
         await t
-        .navigateTo('https://www.directferries.de')
+        .navigateTo('https://www.directferries.de?dealfinderVersion=A')
         .click(Selector('[data-cky-tag="accept-button"]'));
     }
 }
